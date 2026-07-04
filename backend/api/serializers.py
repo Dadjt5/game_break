762c537389e5d153cbf95f671883b49526744399
davi <Dadjt5@gmail.com>
2026-07-04 21:01:23 +0200

    mejoras de conexion

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,26 @@
 from .models import Jugador
 from rest_framework import serializers
+from better_profanity import profanity
+
+
+palabras_censuradas = [
+    "mierda",
+    "joder",
+    "gilipollas",
+    "cabrón",
+    "coño",
+    "puta",
+    "puto"
+]
 
 class JugadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Jugador
-        fields = '__all__'
+        fields = ['id', 'nombreUsuario', 'fechaDeExito']
+
+    def validate_nombreUsuario(self, value):
+        if profanity.contains_profanity(value) or palabras_censuradas.contains(value):
+            raise serializers.ValidationError(
+                "El nombre de usuario contiene contenido no permitido."
+            )
+        return value
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,13 @@
 from .models import Jugador
 from .serializers import JugadorSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 
 class JugadorViewSet(viewsets.ModelViewSet):
     queryset = Jugador.objects.all()
     serializer_class = JugadorSerializer
-    
+
+    def get_permissions(self):
+        if self.action in ['create', 'list', 'retrieve']:
+            return [permissions.AllowAny()]
+        return [permissions.IsAdminUser()]
