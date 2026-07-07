a49875bb0d0c50d3f7828593660b84b3a255911b
Dadjt5 <74069254+Dadjt5@users.noreply.github.com>
2026-07-07 23:31:27 +0200

    Add ScrollToTop component to App

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
     </>
