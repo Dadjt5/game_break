e41f1d3b9cee3db38423e21c951fb8b7782cd583
Dadjt5 <74069254+Dadjt5@users.noreply.github.com>
2026-07-14 12:34:33 +0200

    Refactor App component structure
    Removed unnecessary div wrapper around Outlet and Footer.

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -5,14 +5,9 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
       <ScrollToTop />
       <Navbar />
-
-      <main className="flex-grow-1">
-        <Outlet />
-      </main>
-
+      <Outlet />
       <Footer />
     </div>
   );
